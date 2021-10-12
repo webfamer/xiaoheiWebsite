50d6bba5a25f46a9807cb455a6304831e0284cdc
webfamer321 <webfamer@163.com>
2021-10-13 00:19:56 +0800

    'feature(测试页):添加测试页循环新增软件库‘

---
+++ src/pages/test.vue
@@ -0,0 +1,26 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import softwareJson from "../api/software";
+export default {
+  data() {
+    return {
+      softwareInfo: [],
+    };
+  },
+  methods: {
+    getSoftware() {
+      this.softwareInfo = softwareJson;
+      console.log(this.softwareInfo, "function");
+    },
+  },
+  created () {
+       this.getSoftware();
+  },
+};
+</script>
+
+<style>
+</style>
